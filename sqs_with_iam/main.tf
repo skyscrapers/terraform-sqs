0ed47aa29ada0b066253971face385563fa9767a
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2017-05-10 13:54:41 +0200

    output the sqs id

--- sqs_with_iam/main.tf
+++ sqs_with_iam/main.tf
@@ -5,6 +5,7 @@ data "template_file" "redrive_policy" {
   "maxReceiveCount":$${mrc}
 }
 EOF
+
   vars {
     dlq = "${var.dead_letter_queue}"
     mrc = "${var.max_receive_count}"
@@ -15,8 +16,8 @@ resource "aws_sqs_queue" "queue" {
   name                       = "${var.environment}_${var.project}_${var.name}"
   visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
   delay_seconds              = "${var.delay_seconds}"
-  max_message_size           = "${var.max_message_size}"                       # 256 KB
-  message_retention_seconds  = "${var.message_retention_seconds}"              # 4 days
+  max_message_size           = "${var.max_message_size}"                                                                # 256 KB
+  message_retention_seconds  = "${var.message_retention_seconds}"                                                       # 4 days
   receive_wait_time_seconds  = "${var.receive_wait_time_seconds}"
-  redrive_policy = "${length(var.dead_letter_queue) > 0 ? data.template_file.redrive_policy.rendered : ""}"
+  redrive_policy             = "${length(var.dead_letter_queue) > 0 ? data.template_file.redrive_policy.rendered : ""}"
 }
--- sqs_with_iam/output.tf
+++ sqs_with_iam/output.tf
@@ -2,6 +2,10 @@ output "arn" {
   value = "${aws_sqs_queue.queue.arn}"
 }
 
+output "id" {
+  value = "${aws_sqs_queue.queue.id}"
+}
+
 output "pusher_policy" {
   value = "${aws_iam_policy.pusher_policy.arn}"
 }
